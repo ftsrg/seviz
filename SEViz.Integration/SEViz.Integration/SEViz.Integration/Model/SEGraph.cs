5499680455d7b19dfb30ebbf7f9206863888f631
Dávid Honfi <davidhonfi@gmail.com>
2015-08-21 14:40:37 +0200

    Added symbolic execution graph model and QuickGraph dependency via NuGet.

---
+++ SEViz.Integration/SEViz.Integration/SEViz.Integration/Model/SEGraph.cs
@@ -0,0 +1,13 @@
+﻿using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEViz.Integration.Model
+{
+    public class SEGraph : BidirectionalGraph<SENode, SEEdge>
+    {
+    }
+}
---
+++ SEViz.Integration/SEViz.Integration/SEViz.Integration/Model/SENode.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEViz.Integration.Model
+{
+    public class SENode
+    {
+        public enum NodeColor
+        {
+            White,
+            Green,
+            Orange,
+            Red
+        }
+
+        public enum NodeShape
+        {
+            Ellipse,
+            Rectangle
+        }
+
+        public enum NodeBorder
+        {
+            Single,
+            Double
+        }
+
+        #region Appearance
+
+        public NodeColor Color { get; private set; }
+
+        public NodeShape Shape { get; private set; }
+
+        public NodeBorder Border { get; private set; }
+
+        #endregion
+
+        #region Metadata
+
+        public int Id { get; private set; }
+
+        public string PathCondition { get; private set; }
+
+        public string IncrementalPathCondition { get; private set; }
+
+        public string MethodName { get; private set; }
+
+        public Tuple<string, int> SourceCodeMapping { get; set; }
+
+        public string GenerateTestCode { get; private set; }
+
+        public string Status { get; private set; }
+
+        #endregion
+
+        public SENode(int id, string pathCondition, string methodName, Tuple<string, int> sourceCodeMapping, string generatedTestCode, string status)
+        {
+            // TODO implement constructor for SENode
+        }
+
+    }
+}
