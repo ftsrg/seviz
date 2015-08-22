0324311ff7daad96bac30c4fc88fbb04e962f1cf
Dávid Honfi <davidhonfi@gmail.com>
2015-08-22 17:05:54 +0200

    Renamed ToolWindow title and Button title to 'SEViz'.

--- SEViz.Integration/SEViz.Integration/ViewerWindow.cs
+++ SEViz.Integration/SEViz.Integration/ViewerWindow.cs
@@ -30,7 +30,7 @@ namespace SEViz.Integration
         /// </summary>
         public ViewerWindow() : base(null)
         {
-            this.Caption = "ViewerWindow";
+            this.Caption = "SEViz";
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
             // the object returned by the Content property.
