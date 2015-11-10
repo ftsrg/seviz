84bb2998ac9abb04ddc69daa85bc269f6f150780
Dávid Honfi <davidhonfi@gmail.com>
2015-11-10 10:46:17 +0100

    Filled metadata, added icons.

--- Source/SEViz.Common/Properties/AssemblyInfo.cs
+++ Source/SEViz.Common/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SEViz.Common")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The common library of the SEViz symbolic execution visualization tool.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Budapest University of Technology and Economics")]
 [assembly: AssemblyProduct("SEViz.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
 [assembly: InternalsVisibleTo("SEViz.Integration")]
--- Source/SEViz.Integration/Properties/AssemblyInfo.cs
+++ Source/SEViz.Integration/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SEViz.Integration")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The visualizer component of the SEViz symbolic execution visualization tool.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Budapest University of Technology and Economics")]
 [assembly: AssemblyProduct("SEViz.Integration")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
