f0f62eceba5021fffb2efdaeac50296b576d43c8
Dávid Honfi <davidhonfi@gmail.com>
2015-11-10 08:28:41 +0100

    Added license headers.

--- Source/SEViz.Integration/ViewerWindowCommand.cs
+++ Source/SEViz.Integration/ViewerWindowCommand.cs
@@ -1,8 +1,27 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="ViewerWindowCommand.cs" company="Company">
-//     Copyright (c) Company.  All rights reserved.
-// </copyright>
-//------------------------------------------------------------------------------
+﻿/*
+ * SEViz - Symbolic Execution VIsualiZation
+ *
+ * SEViz is a tool, which can support the test generation process by
+ * visualizing the symbolic execution in a directed graph.
+ *
+ * Authors: Dávid Honfi <honfi@mit.bme.hu>, Zoltán Micskei
+ * <micskeiz@mit.bme.hu>, András Vörös <vori@mit.bme.hu>
+ * 
+ * Copyright 2015 Budapest University of Technology and Economics (BME)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ * 
+ */
 
 using System;
 using System.ComponentModel.Design;
